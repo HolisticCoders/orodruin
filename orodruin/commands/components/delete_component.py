ccc2d675330907c6b0640f043a11a0cbeb27a677
Loïc Pinsard <muream@gmail.com>
2021-09-16 23:05:08 +0200

    disconnect and delete ports before deleting component

--- orodruin/commands/components/delete_component.py
+++ orodruin/commands/components/delete_component.py
@@ -1,10 +1,14 @@
 """Delete Component command."""
 from dataclasses import dataclass, field
+from typing import List, Optional
 from uuid import UUID
+
+from orodruin.utils import list_connections
 
 from ...component import Component
 from ...graph import Graph
 from ..command import Command
+from ..ports import DeletePort, DisconnectPorts
 
 
 @dataclass
@@ -14,15 +18,33 @@ class DeleteComponent(Command):
     graph: Graph
     component_id: UUID
 
-    _deleted_component: Component = field(init=False)
+    _deleted_component: Optional[Component] = field(init=False)
+    _command_stack: List[Command] = field(default_factory=list)
 
     def do(self) -> None:
-        # TODO: Delete all the connectionts from/to this component
-        # TODO: Delete all the Ports from this component
-        self._deleted_component = self.graph.unregister_component(self.component_id)
+        self._deleted_component = self.graph.component_from_uuid(self.component_id)
 
-    def undo(self) -> Component:
-        # TODO: Recreate all the Ports from this component
-        # TODO: Recreate all the connections from/to this component
-        self.graph.register_component(self._deleted_component)
-        return self._deleted_component
+        if not self._deleted_component:
+            return
+
+        for port in self._deleted_component.ports():
+            for connection in list_connections(self.graph, port):
+                command = DisconnectPorts(
+                    self.graph,
+                    connection.source(),
+                    connection.target(),
+                )
+                self._command_stack.append(command)
+                command.do()
+
+            command = DeletePort(self.graph, port.uuid())
+            self._command_stack.append(command)
+            command.do()
+
+        self.graph.unregister_component(self.component_id)
+
+    def undo(self) -> None:
+        if self._deleted_component:
+            for cmd in reversed(self._command_stack):
+                cmd.undo()
+            self.graph.register_component(self._deleted_component)
