2d77bafd62c65743582a396ec90e927f651afc93
Loïc Pinsard <muream@gmail.com>
2021-09-26 22:59:31 +0200

    update group nodes command to be consistent with other commands

--- orodruin/commands/nodes/group_nodes.py
+++ orodruin/commands/nodes/group_nodes.py
@@ -1,9 +1,11 @@
 """Create Node command."""
 from dataclasses import dataclass, field
-from typing import List, Optional
+from typing import List
 
-from orodruin.core import Node, Graph, Library
-from orodruin.core.utils import get_unique_name
+from orodruin.core import Graph, Node
+from orodruin.core.graph import GraphLike
+from orodruin.core.node import NodeLike
+from orodruin.core.state import State
 
 from ..command import Command
 from .create_node import CreateNode
@@ -13,29 +15,27 @@ from .create_node import CreateNode
 class GroupNodes(Command):
     """Create Node command."""
 
-    graph: Graph
-    nodes: List[Node]
+    state: State
+    graph: GraphLike
+    nodes: List[NodeLike]
 
+    _nodes: List[Node] = field(init=False)
+    _graph: Graph = field(init=False)
     _created_node: Node = field(init=False)
-    _command_stack: List[Command] = field(default_factory=list)
+
+    def __post_init__(self) -> None:
+        self._nodes = [self.state.node_from_nodelike(node) for node in self.nodes]
+        self._graph = self.state.graph_from_graphlike(self.graph)
 
     def do(self) -> Node:
-        create_node_command = CreateNode(self.graph, "NewNode")
-        self._command_stack.append(create_node_command)
-        self._created_node = create_node_command.do()
 
-        for node in self.nodes:
-            self.graph.unregister_node(node.uuid())
+        self._created_node = CreateNode(self.state, "NewNode", graph=self._graph).do()
+
+        for node in self._nodes:
+            self._graph.unregister_node(node.uuid())
             self._created_node.graph().register_node(node)
 
         return self._created_node
 
     def undo(self) -> None:
-        # TODO: Delete all the connections from/to this node
-        # TODO: Delete all the Ports from this node
-        self.graph.unregister_node(self._created_node.uuid())
-
-    def redo(self) -> None:
-        # TODO: Delete all the Ports from this node
-        # TODO: Recreate all the connections from/to this node
-        self.graph.register_node(self._created_node)
+        raise NotImplementedError
