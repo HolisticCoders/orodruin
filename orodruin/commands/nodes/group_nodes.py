c02d9d7b551f32508fb5610f234b5c0682100e41
Loïc Pinsard <muream@gmail.com>
2021-10-01 20:12:04 +0200

    Create the minimum amount of input ports when grouping nodes

--- orodruin/commands/nodes/group_nodes.py
+++ orodruin/commands/nodes/group_nodes.py
@@ -1,9 +1,11 @@
 """Create Node command."""
 from dataclasses import dataclass, field
-from typing import List
+from typing import Dict, List
+from uuid import UUID
 
 from orodruin.commands.ports.disconnect_ports import DisconnectPorts
 from orodruin.core import Graph, Node
+from orodruin.core.connection import Connection
 from orodruin.core.graph import GraphLike
 from orodruin.core.node import NodeLike
 from orodruin.core.port.port import PortDirection
@@ -35,6 +37,8 @@ class GroupNodes(Command):
 
         self._created_node = CreateNode(self.state, "NewNode", graph=self._graph).do()
 
+        ingoing_connections: Dict[UUID, List[Connection]] = {}
+
         for node in self._nodes:
             self._graph.unregister_node(node.uuid())
             self._created_node.graph().register_node(node)
@@ -52,35 +56,11 @@ class GroupNodes(Command):
                             self._graph.unregister_connection(connection)
                             self._created_node.graph().register_connection(connection)
                     else:
-                        DisconnectPorts(
-                            self.state,
-                            self._graph,
-                            connection.source(),
-                            connection.target(),
-                        ).do()
                         if port.direction() is PortDirection.input:
                             source = connection.source()
-                            new_port = CreatePort(
-                                self.state,
-                                self._created_node,
-                                port.name(),
-                                PortDirection.input,
-                                port.type(),
-                            ).do()
-                            ConnectPorts(
-                                self.state,
-                                self._graph,
-                                source,
-                                new_port,
-                                force=True,
-                            ).do()
-                            ConnectPorts(
-                                self.state,
-                                self._created_node.graph(),
-                                new_port,
-                                port,
-                                force=True,
-                            ).do()
+                            _connections = ingoing_connections.get(source.uuid(), [])
+                            _connections.append(connection)
+                            ingoing_connections[source.uuid()] = _connections
                         else:
                             target = connection.target()
                             new_port = CreatePort(
@@ -104,6 +84,34 @@ class GroupNodes(Command):
                                 new_port,
                                 force=True,
                             ).do()
+
+        for source_id, connections in ingoing_connections.items():
+            source_port = self.state.port_from_portlike(source_id)
+            new_port = CreatePort(
+                self.state,
+                self._created_node,
+                source_port.name(),
+                PortDirection.input,
+                source_port.type(),
+            ).do()
+            ConnectPorts(
+                self.state,
+                self._graph,
+                source_port,
+                new_port,
+                force=True,
+            ).do()
+            for connection in connections:
+                DisconnectPorts(
+                    self.state, self._graph, connection.source(), connection.target()
+                ).do()
+                ConnectPorts(
+                    self.state,
+                    self._created_node.graph(),
+                    new_port,
+                    connection.target(),
+                    force=True,
+                ).do()
 
         return self._created_node
 
